b48f7511821dbec54d928e04e2403551bfdba409
ShubhaniS <sitoke28@gmail.com>
2025-01-30 19:38:53 +0530

    task-1

--- project/apps/app/src/App.tsx
+++ project/apps/app/src/App.tsx
@@ -1,14 +1,39 @@
-import { List } from 'ui'
+import * as _React from "react";
 
-const api = "https://pokeapi.co/api/v2/pokemon?limit=151"
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
+import { List } from "ui";
+
+const queryClient = new QueryClient();
+
+const fetchPokemon = async () => {
+  const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+  return res.json();
+};
+
+const PokemonList = () => {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["pokemon"],
+    queryFn: fetchPokemon,
+  });
+
+  if (isLoading) return <p>Loading..</p>;
+  if (error instanceof Error) return <p>{error.message}</p>;
+  console.log("data", data.results);
+  // in 2nd task will the result as prop = item={data.results}
+  return <List />;
+};
 
 const App = () => {
   return (
-  <>
-    <h1>Pokemon list:</h1>
-    <List />
-  </>
-  )
-}
+    <QueryClientProvider client={queryClient}>
+      <h1>Pokémon List :</h1>
+      <PokemonList />
+    </QueryClientProvider>
+  );
+};
 
-export default App
+export default App;
